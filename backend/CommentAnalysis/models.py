eb383a89e4098bbae3c94414432003d0cfdedb49
yatharthrawat <rawat.yatharth@gmail.com>
2018-08-07 04:54:44 +0530

    Added .gitignore Created SuperUser 'username' 'helloworld123' Created Model to save analysis output

--- backend/CommentAnalysis/models.py
+++ backend/CommentAnalysis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Analysis (models.Model):
+    Title = models.CharField(max_length=200)
+    url = models.CharField(max_length=1000)
+    description = models.TextField()
+
+    def __str__(self):
+        """ A string representation of the model"""
+        result = "Title: "+ Title + "\n" + "url: " + url + "\n" + "Description: " + description + "\n"
+        return result
+
