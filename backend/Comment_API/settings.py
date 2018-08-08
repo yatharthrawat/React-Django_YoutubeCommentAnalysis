eaae4b53ca25e8daee33d27ff15bca0c36484d00
yatharthrawat <rawat.yatharth@gmail.com>
2018-08-08 05:53:11 +0530

    Changed response output to json

--- backend/Comment_API/settings.py
+++ backend/Comment_API/settings.py
@@ -44,9 +44,18 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+
     'DEFAULT_PERMISSION_CLASSES' : [
         'rest_framework.permissions.AllowAny',
     ]
+
 }
 
 MIDDLEWARE = [
